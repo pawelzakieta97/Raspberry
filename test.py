e68dc15792feae4ce6d83f8f34a839293e86358a
Paweł Żakieta <kanar9123m2@wp.pl>
2017-04-27 21:01:33 +0200

    nowy

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
-import time
-import atexit
-
+from Adafruit_PWM_Servo_Driver import PWM
+ 
 pwm = PWM(0x60)
 pwm.setPWMFreq(1000)
-
-pwm.setPWM(2,0,1000)
+ 
+pwm.setPWM(2, 0, 1000)
+pwm.setPWM(3, 4096, 0)
+pwm.setPWM(4, 0, 0)
