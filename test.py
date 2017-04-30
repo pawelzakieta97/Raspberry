22b25c8615c9180f390db10eabf95525887a13f5
pawelzakieta97 <pawelzakieta97@gmail.com>
2017-04-30 19:41:09 +0200

    1

--- test.py
+++ test.py
@@ -21,6 +21,10 @@ def coil1(pow):
         pwm.setPWM(3, 4096, 0)
         pwm.setPWM(4, 0, 0)
         print ("cewka 1 +")
+    if (pow==0):
+        pwm.setPWM(3, 0, 0)
+        pwm.setPWM(4, 0, 0)
+        print ("cewka 1 null")
     
 def coil2(pow): 
     pwm.setPWM(8, 0, pow)
@@ -32,19 +36,35 @@ def coil2(pow):
         pwm.setPWM(9, 4096, 0)
         pwm.setPWM(10, 0, 0)
         print ("cewka 2 +")
+    if (pow==0):
+        pwm.setPWM(3, 0, 0)
+        pwm.setPWM(4, 0, 0)
+        print ("cewka 2 null")
 
 for i in range (1,iter):
+    coil2(0)
+    coil1(pow)
+    time.sleep(dt)
     coil2(pow)
     coil1(pow)
     time.sleep(dt)
-    coil2(-pow)
-    coil1(pow)
+    coil2(pow)
+    coil1(0)
+    time.sleep(dt)
+    coil2(pow)
+    coil1(-pow)
+    time.sleep(dt)
+    coil2(0)
+    coil1(-pow)
     time.sleep(dt)
     coil2(-pow)
     coil1(-pow)
     time.sleep(dt)
-    coil2(pow)
-    coil1(-pow)
+    coil2(-pow)
+    coil1(0)
+    time.sleep(dt)
+    coil2(-pow)
+    coil1(pow)
     time.sleep(dt)
     
 
