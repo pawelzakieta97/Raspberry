44e8c0065693839c03d0e014b220e610a7084a4e
Paweł Żakieta <kanar9123m2@wp.pl>
2017-04-30 18:39:36 +0200

    nowy

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 #from Adafruit_MotorHAT
-from Adafruit_MotorHAT import Adafruit_MotorHAT
- 
+from Adafruit_MotorHAT.Adafruit_PWM_Servo_Driver import PWM
+
+import atexit
+
+PWMpin=2
+in1=3
+in2=4
+
 pwm = PWM(0x60)
 pwm.setPWMFreq(1000)
  
 pwm.setPWM(2, 0, 1000)
 pwm.setPWM(3, 4096, 0)
 pwm.setPWM(4, 0, 0)
+
+def turnOffMotors():
+    pwm.setPWM(in1,0,0)
+    pwm.setPWM(in2,0,0)
+
+atexit.register(turnOffMotors)
