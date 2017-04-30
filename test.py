dae0007f959200e76f707c6884940e46ed60f4c3
pawelzakieta97 <pawelzakieta97@gmail.com>
2017-04-30 19:15:22 +0200

    1

--- test.py
+++ test.py
@@ -4,16 +4,30 @@ from Adafruit_MotorHAT.Adafruit_PWM_Servo_Driver import PWM
 import atexit
 import time
 
-PWMpin=2
-in1=3
-in2=4
+pow = 1000
+iter = 1000
+
 
 pwm = PWM(0x60)
 pwm.setPWMFreq(1000)
- 
-pwm.setPWM(2, 0, 1000)
-pwm.setPWM(3, 4096, 0)
-pwm.setPWM(4, 0, 0)
+
+def coil1(pow): 
+    pwm.setPWM(2, 0, pow)
+    pwm.setPWM(3, 4096, 0)
+    pwm.setPWM(4, 0, 0)
+    
+def coil2(pow): 
+    pwm.setPWM(8, 0, pow)
+    pwm.setPWM(9, 4096, 0)
+    pwm.setPWM(10, 0, 0)
+
+for i in range (1,iter):
+    coil2(0)
+    coil1(pow)
+    time.sleep(0.01)
+    coil1(0)
+    coil2(pow)
+    time.sleep(0.01)
 
 time.sleep(10)
 
